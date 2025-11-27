40779e94818dec60b2ed52e80f6e5908fde6c867
Israt Jaman Jany <isratjamanjany@Israts-MacBook-Air.local>
2025-11-27 12:13:01 +0600

    destination card data

--- app/components/Destinations.js
+++ app/components/Destinations.js
@@ -7,12 +7,7 @@ export default function Destinations() {
   const [destinations, setDestinations] = useState(null);
   const [error, setError] = useState(null);
 
-  // useEffect(() => {
-  //   fetch("http://localhost:3000/destinations")
-  //     .then((res) => res.json())
-  //     .then((data) => setDestinations(data))
-  //     .catch((err) => setError(err));
-  // }, []);
+
 
   useEffect(() => {
   fetch("http://localhost:3000/destinations")
@@ -35,11 +30,11 @@ export default function Destinations() {
      <div className="container mx-auto p-5">
       <h1 className="text-3xl font-bold mb-6 text-center text-blue-700">Top Destinations</h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
         {destinations.map((item) => (
           <DestinationCard key={item._id} item={item} />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  output: "export", // ⭐ REQUIRED FOR SURGE
+  distDir: "out",
+  images: {
+    unoptimized: true,
+  }
 };
 
 module.exports = nextConfig;
+
